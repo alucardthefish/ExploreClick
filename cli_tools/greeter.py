dc9294c62f0cbb6f5b8cc55643e5e0e6acb27b40
alucardthefish <sergiop18@gmail.com>
2025-06-29 12:47:39 -0500

    Add click option to greeter function in order to allow greetings in two languages Spanish or English

--- cli_tools/greeter.py
+++ cli_tools/greeter.py
@@ -5,6 +5,17 @@ import click
 @click.command()
 @click.argument('first_name')
 @click.argument('last_name')
-def greet(first_name, last_name):
+@click.option(
+    "--lang",
+    help="Specify language English (en) or Spanish (es)",
+    default="en"
+)
+def greet(first_name, last_name, lang):
     """Displays a greeting to the user."""
-    click.echo(f'Hello {first_name} {last_name}!')
+    if lang == "es":
+        greetings = "Hola"
+    elif lang == "en":
+        greetings = "Hello"
+    else:
+        raise click.BadOptionUsage("lang", "lang must be 'en' or 'es'")
+    click.echo(f'{greetings} {first_name} {last_name}!')
