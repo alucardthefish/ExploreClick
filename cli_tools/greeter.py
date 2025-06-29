2d205a54dcfc3578071ef11748e09d2bce10e0c0
alucardthefish <sergiop18@gmail.com>
2025-06-29 12:56:34 -0500

    Refactor code by using validation builtin offer by click with the type of option as a choice

--- cli_tools/greeter.py
+++ cli_tools/greeter.py
@@ -6,16 +6,13 @@ import click
 @click.argument('first_name')
 @click.argument('last_name')
 @click.option(
+    "-l",
     "--lang",
     help="Specify language English (en) or Spanish (es)",
-    default="en"
+    default="en",
+    type=click.Choice(["en", "es"]),
 )
 def greet(first_name, last_name, lang):
     """Displays a greeting to the user."""
-    if lang == "es":
-        greetings = "Hola"
-    elif lang == "en":
-        greetings = "Hello"
-    else:
-        raise click.BadOptionUsage("lang", "lang must be 'en' or 'es'")
+    greetings = "Hello" if lang == "en" else "Hola"
     click.echo(f'{greetings} {first_name} {last_name}!')
